bc48b58c0d5ac1be50f4fd59fc8449e8fc28d66d
abw1003 <abw1003@gmail.com>
2024-01-18 15:47:05 +0900

    주석 추가

--- 01-helloworld.py
+++ 01-helloworld.py
@@ -1,1 +1,14 @@
-print("aaa")
+print("주석 내용 확인")
+
+
+#git add .\01-helloworld.py
+#git add .
+#git commit -m "업데이트 제목"
+#git push origin main(branch 명)
+
+# git 기본 명령어
+# echo "# python-git" >> README.md  # README.md 파일을 생성하고 내용을 추가합니다.
+# git init  # Git 저장소를 초기화합니다.
+# git add README.md  # README.md 파일을 스테이징 영역에 추가합니다.
+# git commit -m "first commit"  # 스테이징된 변경사항을 커밋합니다.
+# git branch -M main  # 기본 브랜치의 이름을 'main'으로 변경합니다.

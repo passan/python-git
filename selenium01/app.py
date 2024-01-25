fe818fea7a24e0acbd40f048cd229317f1353044
abw1003 <abw1003@gmail.com>
2024-01-25 10:52:28 +0900

    copy selector 로 텍스트 가지고 오기

--- selenium01/app.py
+++ selenium01/app.py
@@ -1,4 +1,6 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By #class by사용
 
 import chromedriver_autoinstaller
 
@@ -6,6 +8,11 @@ chromedriver_autoinstaller.install()
 
 driver = webdriver.Chrome()
 driver.get("http://www.naver.com")
+time.sleep(1)
 
+css_selecter = "#shortcutArea > ul" #copy selector
+group_aavigator = driver.find_element(By.CSS_SELECTOR, css_selecter)
+
+print(group_aavigator.text)
 
 input()

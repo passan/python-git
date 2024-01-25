7aa678eba73034a912c28037772ec2c09fcbc02f
abw1003 <abw1003@gmail.com>
2024-01-25 11:20:04 +0900

    selector에서 클릭까지 추가

--- selenium01/app.py
+++ selenium01/app.py
@@ -7,12 +7,19 @@ import chromedriver_autoinstaller
 chromedriver_autoinstaller.install()
 
 driver = webdriver.Chrome()
+# 1. 웹 브라우저 주소창을 컨트롤하기 driver.get
 driver.get("http://www.naver.com")
 time.sleep(1)
 
+# 2-1. 요소를 찾아서 Copy 해옴. 실제 웹 브라우저 개발자 도수로 찾아서 사용해야함.
 css_selecter = "#shortcutArea > ul" #copy selector
-group_aavigator = driver.find_element(By.CSS_SELECTOR, css_selecter)
 
-print(group_aavigator.text)
+# 2-2. 찾아온 요소를 find_element로 가져오기
+group_navigator = driver.find_element(By.CSS_SELECTOR, css_selecter)
 
+# 3-1. 데이터를 가져오기
+print(group_navigator.text)
+
+# 3-2. 요소를 클릭하기 
+group_navigator.click()
 input()

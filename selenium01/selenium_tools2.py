fd007af359def54c901aa3b6404aa58c696502b1
abw1003 <abw1003@gmail.com>
2024-01-25 12:53:28 +0900

    url에 로그인 주소가 있으면 if문 실행

--- selenium01/selenium_tools2.py
+++ selenium01/selenium_tools2.py
@@ -8,7 +8,7 @@ chromedriver_autoinstaller.install()
 
 driver = webdriver.Chrome()
 
-driver.get("https://naver.com")
+driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/")
 # 2-1. browser infomation
 title = driver.title
 print(title, "이 타이틀이다.")
@@ -16,3 +16,10 @@ print(title, "이 타이틀이다.")
 # 2-2. current_url 주소창을 그대로 가지고옴
 driver.current_url
 print(driver.current_url, "가 현재주소다")
+
+if "nid.naver.com" in driver.current_url:
+    print("지금은 로그인 로직이 필요함")
+else:
+    print("내가 계획한 로직 그대로 실행하면됨")
+
+input() 

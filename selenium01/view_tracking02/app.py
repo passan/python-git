97e84d0c3a1dd4e4ccf009b9d524d5a48160072c
abw1003 <abw1003@gmail.com>
2024-02-01 18:15:20 +0900

    for문 추가

--- selenium01/view_tracking02/app.py
+++ selenium01/view_tracking02/app.py
@@ -9,32 +9,36 @@ chromedriver_autoinstaller.install()
 
 driver = webdriver.Chrome()
 
-pagingindex = 2
-seach_query = "꿀사과"
-shoping_link = f"https://msearch.shopping.naver.com/search/all?query={seach_query}&vertical=search&pagingIndex={pagingindex}"
-driver.get(shoping_link)
+real_rank = -1
+rank = 1
+for pagingindex in range(1, 15):
+    # pagingindex = 2 # for문 추가로 사용 안 함
+    seach_query = "꿀사과"
+    shoping_link = f"https://msearch.shopping.naver.com/search/all?query={seach_query}&vertical=search&pagingIndex={pagingindex}"
+    driver.get(shoping_link)
 
-# 스크롤을 내립니다.
-for _ in range(5):
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(2)
+    # 스크롤을 내립니다.
+    for _ in range(5):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
 
-# 상품 코드를 찾습니다.
-try:
-    target_code = "83450430618"
-    target_selector = f"a[data-i='{target_code}']"
-    target_element = driver.find_element(By.CSS_SELECTOR, target_selector)
-    data = target_element.get_attribute("data-nclick")
-    print(data)# 전체 상품 코드 출력
-    print(data.split(f"{target_code},r:")) # 상품 코드 빼고 r을 기준으로 리스트를 만들어 출력 코드는 출력 안됨
-    print(data.split(f"{target_code},r:")[-1]) # 리스트에 마지막 요소만 출력
-    rank = (data.split(f"{target_code},r:")[-1].split(",")[0]) # 0번 인덱스 요소만 출력
-    print("등수", rank) #현제 페이지 등수
-    # real_rank = (int(pagingindex) - 1) * 40 + int(rank) #페이지별로 다시 순번이 안정해지기 때문에 사용 안함
-    
-except:
-    print("상품 코드를 찾을 수 없습니다.")    
-
+    # 상품 코드를 찾습니다.
+    try:
+        target_code = "83450430618"
+        target_selector = f"a[data-i='{target_code}']"
+        target_element = driver.find_element(By.CSS_SELECTOR, target_selector)
+        data = target_element.get_attribute("data-nclick")
+        print(data)# 전체 상품 코드 출력
+        print(data.split(f"{target_code},r:")) # 상품 코드 빼고 r을 기준으로 리스트를 만들어 출력 코드는 출력 안됨
+        print(data.split(f"{target_code},r:")[-1]) # 리스트에 마지막 요소만 출력
+        rank = (data.split(f"{target_code},r:")[-1].split(",")[0]) # 0번 인덱스 요소만 출력
+        print("등수", rank) #현제 페이지 등수
+        real_rank = int(pagingindex) - (int(pagingindex) -1) * 40 + int(rank) #진짜 등수
+        break        
+    except:
+        print(f"{pagingindex}페이지에서 상품 코드를 찾을 수 없습니다.")    
+print("상품 진짜 등수는 ", real_rank , "등입니다.")
+print(f"상품은 {pagingindex} 페이지의 {rank}등에 노출되고 있습니다.")
 
 
 input()
